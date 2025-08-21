db14d6e734e657fe55c24aa3d9179ea1c29bfdd2
Erston Greatman <erston.greatman@gmail.com>
2025-08-21 12:04:09 -0600

    Better presence for Game Activity - If playing a game, tries to show details and state (and party if that data exists) - If streaming, tries to match the game in Streaming state with the game in Discord activities. If found, tries to display game info

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -24,13 +24,22 @@ export const usePrimaryActivity = (activities: Activity[] | undefined): Activity
     // Let's only return the first streaming service for now
     // (I don't even think Discord shows anything other than Twitch anyway)
     const activity = isStreaming[0];
+    const [game] = activities.filter(game => game.name === activity.state);
+    const baseText = [
+      `Streaming on ${activity.name}`,
+      activity.details ?? '',
+      `${activity.state}`,
+    ];
+
     return {
       activityAction: 'Streaming',
       name: activity.name,
       text: [
-        `Streaming on ${activity.name}`,
-        activity.details ?? '',
-        `${activity.state}`,
+        ...baseText,
+        ...(game ? [
+          `${game.details ? game.details : ''}`,
+          `${game.state}${game.party ? ` (${game.party.size?.[0]} of ${game.party.size?.[1]})` : ''}`,
+        ] : []),
       ],
       url: activity.url,
     };
@@ -43,6 +52,7 @@ export const usePrimaryActivity = (activities: Activity[] | undefined): Activity
     name: activity.name,
     text: [
       `Playing ${activity.name}`,
+      `${activity.state}${activity.party ? ` (${activity.party.size?.[0]} of ${activity.party.size?.[1]})` : ''}`,
     ],
   };
 };
